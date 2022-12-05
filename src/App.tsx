9343ddf75d0b78e2271f77bca67f448773cc2411
Noel Portugal <noelportugal@gmail.com>
2022-12-05 12:28:05 -1000

    blaj

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GraphiQL from "graphiql";
+import "graphiql/graphiql.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <GraphiQL
+    fetcher={async (graphQLParams) => {
+      const data = await fetch(
+        "https://swapi-graphql.netlify.app/.netlify/functions/index",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(graphQLParams),
+          credentials: "same-origin",
+        }
+      );
+      return data.json().catch(() => data.text());
+    }}
+  />
+);
 
 export default App;
